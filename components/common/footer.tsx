9df186c4fb48c3bf20fabe0610b9c1083309db47
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-10-17 22:33:46 +0300

    feat: add Footer component with external link and dynamic year
    - Created `Footer` component with responsive layout and accessibility features
    - Added external link to GitHub profile with focus-visible and hover states
    - Included dynamic display of the current year using `Date().getFullYear()`
    - Implemented Next.js `Image` component for optimized image rendering

---
+++ components/common/footer.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import Image from 'next/image';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className='footer row-start-3 w-full'>
+      <div className='mx-auto flex w-full max-w-5xl items-center justify-between'>
+        {' '}
+        <a
+          className='custom-transition custom-transition peer-focus-visible:ring-ring/20 relative flex cursor-pointer items-center justify-center gap-2 rounded-lg border border-transparent bg-background p-2 ring-offset-background transition-shadow hover:text-accent hover:underline hover:underline-offset-4 focus-visible:border-ring focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2'
+          href='https://github.com/Iryna-Vyshniak'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <Image
+            aria-hidden
+            src='https://nextjs.org/icons/globe.svg'
+            alt='Globe icon'
+            width={16}
+            height={16}
+          />
+          Go to →
+        </a>{' '}
+        <p>Form Elements UI - {new Date().getFullYear()}</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
