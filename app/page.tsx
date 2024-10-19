9d0ea75a29a6858a43e7726217ee3a14580ed2aa
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-10-19 14:33:51 +0300

    feat: add animated Logo and preloader transition to Home component
    - Integrated `Logo` component with animated blur effect using `framer-motion` in the Home layout
    - Added preloader animation that slides out the `Logo` after 20 seconds, enhancing the entry experience
    - Updated layout structure to include an animated preloader before rendering form elements
    - Kept form components (`SimpleEmailInput`, `RequiredInput`, `InputWithHelper`, `TagInput`) unchanged

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
+import * as motion from 'framer-motion/client';
+
+import { BLUR_VARIANT } from '@/lib/motion-variants';
+
+import Logo from '@/components/common/logo';
 import InputWithHelper from '@/components/elements/input-with-helper';
 import RequiredInput from '@/components/elements/required-input';
 import SimpleEmailInput from '@/components/elements/simple-email-input';
@@ -5,11 +10,26 @@ import TagInput from '@/components/elements/tag-input';
 
 export default function Home() {
   return (
-    <main className='[&>*]:before:bg-border/70 [&>*]:after:bg-border/70 grid max-w-6xl grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 [&>*]:relative [&>*]:px-2 [&>*]:py-8 [&>*]:before:absolute [&>*]:before:[block-size:100vh] [&>*]:before:[inline-size:1px] [&>*]:before:[inset-block-start:0] [&>*]:before:[inset-inline-start:-1px] [&>*]:after:absolute [&>*]:after:[block-size:1px] [&>*]:after:[inline-size:100vw] [&>*]:after:[inset-block-start:-1px] [&>*]:after:[inset-inline-start:0] sm:[&>*]:px-8 xl:[&>*]:px-12'>
-      <SimpleEmailInput />
-      <RequiredInput />
-      <InputWithHelper />
-      <TagInput />
-    </main>
+    <>
+      <motion.div
+        initial={{ top: '0%' }}
+        animate={{ top: '-100%' }}
+        transition={{ duration: 20, delay: 0.8, ease: 'easeInOut' }}
+        className='absolute top-0 z-[100] flex h-screen w-screen items-center justify-center bg-accent'
+      >
+        <Logo
+          fontSize={1.2}
+          radius={7}
+          itemVariant={BLUR_VARIANT}
+          text='form • elements • ui • '
+        />
+      </motion.div>{' '}
+      <main className='[&>*]:before:bg-border/70 [&>*]:after:bg-border/70 grid max-w-6xl grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 [&>*]:relative [&>*]:px-2 [&>*]:py-8 [&>*]:before:absolute [&>*]:before:[block-size:100vh] [&>*]:before:[inline-size:1px] [&>*]:before:[inset-block-start:0] [&>*]:before:[inset-inline-start:-1px] [&>*]:after:absolute [&>*]:after:[block-size:1px] [&>*]:after:[inline-size:100vw] [&>*]:after:[inset-block-start:-1px] [&>*]:after:[inset-inline-start:0] sm:[&>*]:px-8 xl:[&>*]:px-12'>
+        <SimpleEmailInput />
+        <RequiredInput />
+        <InputWithHelper />
+        <TagInput />
+      </main>
+    </>
   );
 }
